7377a687c22030bbbff86e90ee2da28f52ed85ab
giray sam <gry.sam@gmail.com>
2016-11-10 14:34:37 +0200

    add: encryption feature for data

--- test/boomerang-cache_tests.js
+++ test/boomerang-cache_tests.js
@@ -15,6 +15,20 @@ catch (ex) {
     console.log(ex);
 }
 
+try {
+    var boomerangLocalEncrypt = BoomerangCache.create("boomerangLocalEncrypt", {encrypt: true});
+}
+catch (ex) {
+    console.log(ex);
+}
+
+try {
+    var boomerangSessionEncrypt = BoomerangCache.create("boomerangSessionEncrypt", {storage: 'session', encrypt: true});
+}
+catch (ex) {
+    console.log(ex);
+}
+
 describe('LocalStorage Tests', function () {
 
     beforeEach(function() {
@@ -232,3 +246,219 @@ describe('SessionStorage Tests', function () {
     });
 });
 
+
+describe('LocalStorage Tests with Encryption', function () {
+
+    beforeEach(function() {
+        try {
+            localStorage.clear();
+        }
+        catch (ex) {}
+    });
+
+    afterEach(function() {
+        try {
+            localStorage.clear();
+        }
+        catch (ex) {}
+    });
+
+    it('Testing set() and get()', function () {
+        var key = "boomerang_key",
+            value = "boomerang_value";
+
+        boomerangLocalEncrypt.set(key, value);
+        assert.equal(boomerangLocalEncrypt.get(key), value, "We expect value to be " + value);
+    });
+
+    it('Testing number with set() and get()', function() {
+        var key = 'boomerang_array',
+            value = 123456789;
+
+        boomerangLocalEncrypt.set(key, value);
+        assert.equal(boomerangLocalEncrypt.get(key), value, "We expect value to be " + value);
+    });
+
+    it('Testing array with set() and get()', function() {
+        var key = 'boomerang_array',
+            value = [{value: "boomerang_value"}];
+
+        boomerangLocalEncrypt.set(key, value);
+        assert.deepEqual(boomerangLocalEncrypt.get(key), value, "We expect type of value to be " + typeof(value));
+    });
+
+    it('Testing object with set() and get()', function() {
+        var key = 'boomerang_object',
+            value = {value: "boomerang_value"};
+
+        boomerangLocalEncrypt.set(key, value);
+        assert.deepEqual(boomerangLocalEncrypt.get(key), value, "We expect type of value to be " + typeof(value));
+    });
+
+    it('Testing set() has expire', function() {
+        var key = 'boomerang_object',
+            value = "boomerang_value";
+
+        boomerangLocalEncrypt.set(key, value, -1);
+        assert.equal(boomerangLocalEncrypt.get(key), null, "We expect type of value to be null");
+    });
+
+    it('Testing set() has not expire', function() {
+        var key = 'boomerang_object',
+            value = "boomerang_value";
+
+        boomerangLocalEncrypt.set(key, value, 1);
+        assert.notEqual(boomerangLocalEncrypt.get(key), null, "We expect type of value to be not null");
+    });
+
+    it('Testing getAll()', function () {
+        var key_1 = 'boomerang_object_1',
+            value_1 = "boomerang_value_1",
+            key_2 = 'boomerang_object_2',
+            value_2 = "boomerang_value_2";
+
+        boomerangLocalEncrypt.set(key_1, value_1);
+        boomerangLocalEncrypt.set(key_2, value_2);
+
+        var items = boomerangLocalEncrypt.getAll();
+
+        assert.equal(items[key_1], value_1, 'We expect value to be ' + value_1);
+        assert.equal(items[key_2], value_2, 'We expect value to be ' + value_2);
+
+    });
+
+    it('Testing length()', function() {
+        var key = 'boomerang_object',
+            value = "boomerang_value";
+
+        boomerangLocalEncrypt.clear();
+        boomerangLocalEncrypt.set(key, value);
+        assert.equal(boomerangLocalEncrypt.length(), 1, "We expect type of value to be 1");
+    });
+
+    it('Testing remove()', function() {
+        var key = 'boomerang_key',
+            value = "boomerang_value";
+
+        boomerangLocalEncrypt.set(key, value);
+        boomerangLocalEncrypt.remove(key);
+        assert.equal(boomerangLocalEncrypt.get(key), null, 'We expect value to be null');
+    });
+
+    it('Testing clear()', function() {
+        var key = 'boomerang_key',
+            value = "boomerang_value";
+
+        boomerangLocalEncrypt.set(key, value);
+        boomerangLocalEncrypt.clear();
+        assert.equal(boomerangLocalEncrypt.get(key), null, 'We expect value to be null');
+    });
+});
+
+describe('SessionStorage Tests with Encryption', function () {
+
+    beforeEach(function() {
+        try {
+            localStorage.clear();
+        }
+        catch (ex) {}
+    });
+
+    afterEach(function() {
+        try {
+            localStorage.clear();
+        }
+        catch (ex) {}
+    });
+
+    it('Testing set() and get()', function () {
+        var key = "boomerang_key",
+            value = "boomerang_value";
+
+        boomerangSessionEncrypt.set(key, value);
+        assert.equal(boomerangSessionEncrypt.get(key), value, "We expect value to be " + value);
+    });
+
+    it('Testing number with set() and get()', function() {
+        var key = 'boomerang_array',
+            value = 123456789;
+
+        boomerangSessionEncrypt.set(key, value);
+        assert.equal(boomerangSessionEncrypt.get(key), value, "We expect value to be " + value);
+    });
+
+    it('Testing array with set() and get()', function() {
+        var key = 'boomerang_array',
+            value = [{value: "boomerang_value"}];
+
+        boomerangSessionEncrypt.set(key, value);
+        assert.deepEqual(boomerangSessionEncrypt.get(key), value, "We expect type of value to be " + typeof(value));
+    });
+
+    it('Testing object with set() and get()', function() {
+        var key = 'boomerang_object',
+            value = {value: "boomerang_value"};
+
+        boomerangSessionEncrypt.set(key, value);
+        assert.deepEqual(boomerangSessionEncrypt.get(key), value, "We expect type of value to be " + typeof(value));
+    });
+
+    it('Testing set() has expire', function() {
+        var key = 'boomerang_object',
+            value = "boomerang_value";
+
+        boomerangSessionEncrypt.set(key, value, -1);
+        assert.equal(boomerangSessionEncrypt.get(key), null, "We expect type of value to be null");
+    });
+
+    it('Testing set() has not expire', function() {
+        var key = 'boomerang_object',
+            value = "boomerang_value";
+
+        boomerangSessionEncrypt.set(key, value, 1);
+        assert.notEqual(boomerangSessionEncrypt.get(key), null, "We expect type of value to be not null");
+    });
+
+    it('Testing getAll()', function () {
+        var key_1 = 'boomerang_object_1',
+            value_1 = "boomerang_value_1",
+            key_2 = 'boomerang_object_2',
+            value_2 = "boomerang_value_2";
+
+        boomerangSessionEncrypt.set(key_1, value_1);
+        boomerangSessionEncrypt.set(key_2, value_2);
+
+        var items = boomerangSessionEncrypt.getAll();
+
+        assert.equal(items[key_1], value_1, 'We expect value to be ' + value_1);
+        assert.equal(items[key_2], value_2, 'We expect value to be ' + value_2);
+
+    });
+
+    it('Testing length()', function() {
+        var key = 'boomerang_object',
+            value = "boomerang_value";
+
+        boomerangSessionEncrypt.clear();
+        boomerangSessionEncrypt.set(key, value);
+        assert.equal(boomerangSessionEncrypt.length(), 1, "We expect type of value to be 1");
+    });
+
+    it('Testing remove()', function() {
+        var key = 'boomerang_key',
+            value = "boomerang_value";
+
+        boomerangSessionEncrypt.set(key, value);
+        boomerangSessionEncrypt.remove(key);
+        assert.equal(boomerangSessionEncrypt.get(key), null, 'We expect value to be null');
+    });
+
+    it('Testing clear()', function() {
+        var key = 'boomerang_key',
+            value = "boomerang_value";
+
+        boomerangSessionEncrypt.set(key, value);
+        boomerangSessionEncrypt.clear();
+        assert.equal(boomerangSessionEncrypt.get(key), null, 'We expect value to be null');
+    });
+});
